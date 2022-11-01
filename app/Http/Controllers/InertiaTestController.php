0ef93abd54e39d291fdb5983e4d9ba07532d686e
Mizuki Okushima <>
2022-11-01 10:16:53 +0900

    inertia-vue.js

---
+++ app/Http/Controllers/InertiaTestController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use App\Models\InertiaTest;
+
+class InertiaTestController extends Controller
+{
+    public function index() {
+        return Inertia::render('Inertia/index', [
+            // InertiaTest::all() テーブルすべての情報を取得
+            'blogs' => InertiaTest::all()
+        ]);
+    }
+
+    public function create() {
+        return Inertia::render('Inertia/Create');
+    }
+
+    public function show($id) {
+        // ddでLaravel側のでのデバッグ
+        // dd($id);
+
+        return Inertia::render('Inertia/Show',
+        [
+            'id' => $id,
+            'blog' => InertiaTest::findOrFail($id)
+        ]);
+    }
+
+    public function store(Request $request) {
+        // $requestでinputタグに入力した値を変数で受けとることができる。
+
+        // バリデーション処理
+        $request->validate([
+            'title' => ['required', 'max:20'],
+            'content' => ['required'],
+        ]);
+
+        // 上記のuse文のモデルのインスタンス化
+        $inertiaTest = new InertiaTest;
+        $inertiaTest->title = $request->title;
+        $inertiaTest->content = $request->content;
+        $inertiaTest->save();
+
+        // Laravelにてリダイレクト処理
+        // ->withでフラッシュメッセージを渡す
+        return to_route('inertia.index')->with([
+            'message' => '登録しました。'
+        ]);
+    }
+
+    public function delete($id) {
+
+        // $book 1件だけの情報指定
+        $book = InertiaTest::findOrFail($id);
+        // 1件だけ削除 Laravelの機能
+        $book->delete();
+
+        return to_route('inertia.index')->with([
+            'message' => '削除しました。'
+        ]);
+    }
+}
---
+++ resources/js/Pages/InertiaTest.vue
@@ -0,0 +1,31 @@
+
+<script setup>
+import { Link } from '@inertiajs/inertia-vue3';
+import { ref } from 'vue'
+
+const newTitle = ref('')
+const newContent = ref('')
+
+</script>
+
+<template>
+Inertiaテストです。<br>
+<a href="/">aタグ経由です</a><br>
+<Link href="/">Link経由です</Link><br>
+<!-- :hrefでvue.jsのv-bindが使える リンクを動的に使える -->
+<Link :href="route('inertia.index')">名前付きのルートの確認です</Link><br>
+<Link :href="route('inertia.show', { id: 50 })">ルートパラメータのテストです</Link><br>
+
+<div class-="mb-8"></div>
+
+<!-- v-modelで入力した値を変数に代入する -->
+<!-- {{ newTitle }}でリアルタイムで入力値を表示する -->
+<input type="text" name="newTitle" v-model="newTitle">{{ newTitle }}<br>
+<input type="text" name="newContent" v-model="newContent">{{ newContent }}<br>
+<!-- methodはpostを指定 -->
+<Link as="button" method="post" :href="route('inertia.store')"
+:data="{
+    title: newTitle,
+    content: newContent
+}">BD保存テスト</Link>
+</template>
